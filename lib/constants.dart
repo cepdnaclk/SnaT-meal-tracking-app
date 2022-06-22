41922a3b605e77fef085d6c71f0bebdc0dbbedd2
Hari25483 <haricodmw@gmail.com>
2022-06-22 11:35:00 +0530

    Update Introduction Text

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,6 +1,6 @@
 String appName = "SnaT";
 String introduction =
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo lectus, ac blandit";
+    "Experience next-level food tracking with photos, emojis and fun-filled food journaling... Your way to a balanced meal is just a click or two away...";
 
 String loginImage = "assets/images/login.jpg";
 
