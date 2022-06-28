f6f16508daefdeae0bb969f4fc6ad9b742d76caa
Yogeswaran Thulasiyan <73667144+SyThulasi@users.noreply.github.com>
2022-06-28 18:40:07 +0530

    added chart page

--- lib/Theme/theme_info.dart
+++ lib/Theme/theme_info.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 
 class ThemeInfo {
   //Colors
-  static Color primaryColor = const Color(0xffb4f2ec);
+  static Color primaryColor = const Color(0xff0f5951);
+  //static Color primaryColor = const Color(0xffb4f2ec);
   static Color bottomTabButtonColor = const Color(0xff2ea29f);
   // Fonts
   static String logoFontFamily = "Abril Fatface";
