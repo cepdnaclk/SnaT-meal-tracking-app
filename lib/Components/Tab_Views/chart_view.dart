7ed90a415beae083a0577da5d7f81d03f5868c20
Yogeswaran Thulasiyan <73667144+SyThulasi@users.noreply.github.com>
2022-07-02 22:49:52 +0530

    pie chart removed

--- lib/Components/Tab_Views/chart_view.dart
+++ lib/Components/Tab_Views/chart_view.dart
@@ -3,13 +3,11 @@ import 'package:charts_flutter/flutter.dart' as charts;
 import 'dart:io';
 import 'dart:typed_data';
 import 'package:image_gallery_saver/image_gallery_saver.dart';
-import 'package:mobile_app/Theme/theme_info.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:screenshot/screenshot.dart';
 import 'package:share_plus/share_plus.dart';
 import 'chart_view_components/barchart_model.dart';
-import 'chart_view_components/legend_container.dart';
 import 'chart_view_components/pieChart_model.dart';
 import 'chart_view_components/share_button.dart';
 
@@ -90,172 +88,40 @@ class _ChartViewState extends State<ChartView> {
             padding: const EdgeInsets.all(10.0),
             child: Column(
               children: [
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.grey,
-                    borderRadius: BorderRadius.circular(20.0),
-                  ),
-                  child: TabBar(
-                    indicator: BoxDecoration(
-                        color: ThemeInfo.primaryColor,
-                        borderRadius: BorderRadius.circular(20.0)),
-                    indicatorColor: const Color(0xff1976d2),
-                    tabs: const [
-                      Tab(text: "For Today"),
-                      Tab(text: "This Week"),
-                    ],
+                SizedBox(
+                  height: size.height * 0.075,
+                  width: size.width * 0.9,
+                  child: Container(
+                    alignment: Alignment.center,
+                    child: const Text(
+                      "Total intake for this week",
+                      style: TextStyle(
+                        fontSize: 25.0,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xff0f5951),
+                      ),
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.grey.shade400,
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
                   ),
                 ),
                 SizedBox(
-                  height: size.height * 0.69,
-                  child: TabBarView(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(10.0),
-                        child: Container(
-                          padding: const EdgeInsets.all(3.0),
-                          decoration: BoxDecoration(
-                              color: Colors.grey.shade300,
-                              borderRadius: BorderRadius.circular(20.0)),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: <Widget>[
-                              SizedBox(
-                                height: size.height * 0.01,
-                              ),
-                              const Text(
-                                'Daily Nutrients intake',
-                                style: TextStyle(
-                                    fontSize: 20.0,
-                                    fontWeight: FontWeight.bold,
-                                    color: Color(0xff0f5951)),
-                              ),
-                              SizedBox(
-                                height: size.height * 0.02,
-                              ),
-                              Container(
-                                height: size.height * 0.13,
-                                width: size.width * 0.85,
-                                decoration: BoxDecoration(
-                                  color: Colors.grey.shade400,
-                                  borderRadius: BorderRadius.circular(10.0),
-                                  boxShadow: [
-                                    BoxShadow(
-                                      color: Colors.grey.shade500,
-                                      offset: const Offset(0.0, 5.0),
-                                      blurRadius: 5.0,
-                                      spreadRadius: 1.0,
-                                    ),
-                                    const BoxShadow(
-                                        color: Colors.white,
-                                        offset: Offset(0.0, 0.0))
-                                  ],
-                                ),
-                                child: Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceEvenly,
-                                  children: <Widget>[
-                                    Column(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.spaceEvenly,
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: const <Widget>[
-                                        Legend(
-                                          colour: Color(0xff054840),
-                                          text: 'Cereals and Starchy foods',
-                                        ),
-                                        Legend(
-                                          colour: Color(0xff16867a),
-                                          text: 'Vegetables',
-                                        ),
-                                        Legend(
-                                          colour: Color(0xff85dad0),
-                                          text: 'Fruit',
-                                        ),
-                                      ],
-                                    ),
-                                    SizedBox(
-                                      width: 2,
-                                      height: size.height * 0.12,
-                                      child: DecoratedBox(
-                                        decoration: BoxDecoration(
-                                          color: Colors.grey.shade500,
-                                        ),
-                                      ),
-                                    ),
-                                    Column(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.spaceEvenly,
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: const <Widget>[
-                                        Legend(
-                                          colour: Color(0xffb1dad6),
-                                          text: 'Pulses Meat Fish',
-                                        ),
-                                        Legend(
-                                          colour: Color(0xff7a7979),
-                                          text: 'Beverages',
-                                        ),
-                                        Legend(
-                                          colour: Color(0xff000000),
-                                          text: 'Milk and Milk Products',
-                                        ),
-                                      ],
-                                    ),
-                                  ],
-                                ),
-                              ),
-                              Expanded(
-                                child: charts.PieChart<String>(
-                                  _seriesPieData,
-                                  animate: true,
-                                  animationDuration: const Duration(seconds: 1),
-                                  defaultRenderer: charts.ArcRendererConfig(
-                                    arcWidth: 75,
-                                    arcRendererDecorators: [
-                                      charts.ArcLabelDecorator(
-                                          labelPosition:
-                                              charts.ArcLabelPosition.inside)
-                                    ],
-                                  ),
-                                ),
-                              ),
-                              Button(
-                                press: () async {
-                                  final image = await controller.capture();
-                                  if (image == null) return;
-
-                                  await saveImage(image);
-                                  await saveAndShare(image);
-                                },
-                              ),
-                            ],
-                          ),
+                    height: size.height * 0.69,
+                    width: size.width * 0.95,
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20.0),
+                          color: Colors.grey.shade300,
                         ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(10.0),
-                        child: Container(
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(20.0),
-                            color: Colors.grey.shade300,
-                          ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(10.0),
                           child: Column(
                             crossAxisAlignment: CrossAxisAlignment.center,
                             children: <Widget>[
-                              SizedBox(
-                                height: size.height * 0.01,
-                              ),
-                              const Text(
-                                "Total intake for this week",
-                                style: TextStyle(
-                                  fontSize: 20.0,
-                                  fontWeight: FontWeight.bold,
-                                  color: Color(0xff0f5951),
-                                ),
-                              ),
                               SizedBox(
                                 height: size.height * 0.01,
                               ),
@@ -324,10 +190,8 @@ class _ChartViewState extends State<ChartView> {
                             ],
                           ),
                         ),
-                      )
-                    ],
-                  ),
-                ),
+                      ),
+                    )),
               ],
             ),
           ),
