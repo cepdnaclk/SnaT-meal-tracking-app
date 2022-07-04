de62805cbcd284e681c0cb84434ac674ac6abbf0
SathsaraniAththanayaka <e18030@eng.pdn.ac.lk>
2022-07-05 04:27:41 +0530

    Update login

--- lib/Pages/login_screen.dart
+++ lib/Pages/login_screen.dart
@@ -26,66 +26,119 @@ class LoginScreen extends StatelessWidget {
                 Container(
                   width: MediaQuery.of(context).size.width,
                   color: ThemeInfo.primaryColor,
-                  child: Column(
-                    children: [
-                      SizedBox(
-                        height: 20,
-                      ),
-                      ClipRRect(
-                        borderRadius: BorderRadius.circular(20),
-                        child: Image.asset(loginImage,
-                            width: MediaQuery.of(context).size.width - 60,
-                            height: MediaQuery.of(context).size.width - 60),
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                        child: Text(
-                          introduction,
-                          style: TextStyle(fontSize: 15),
-                          textAlign: TextAlign.center,
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          height: 40,
                         ),
-                      ),
-                    ],
-                  ),
-                ),
-                Positioned(
-                  bottom: -25,
-                  child: Container(
-                    width: MediaQuery.of(context).size.width,
-                    alignment: Alignment.center,
-                    child: ElevatedButton(
-                      onPressed: () async {
-                        await FirebaseServices().signInWithGoogle();
-                        print("Hello");
-                        Navigator.of(context).push(CustomPageRoute(
-                            //child: DashboardLayout(),
-                            //child: AdditionalSettingsScreen(),
-                            child: RegisterPage(),
-                            transition: "slide right"));
-                      },
-                      child: const Padding(
-                        padding: EdgeInsets.symmetric(
-                            vertical: 15.0, horizontal: 10),
-                        child: Text(
-                          "Sign in with Google",
-                          style: TextStyle(fontSize: 20),
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(20),
+                          child: Image.asset(loginImage,
+                              width: MediaQuery.of(context).size.width - 70,
+                              height: MediaQuery.of(context).size.width - 70),
                         ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                          child: Text(
+                            introduction,
+                            style: TextStyle(fontSize: 15,color:Colors.white),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                  
+                        SizedBox(height:20.0),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            ElevatedButton(
+                              onPressed: () async{
+                                //Navigator.push(context, MaterialPageRoute(builder : (context) => SignIn()));
+                              },
+                              child: Text('  Sign In  ',style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black,
+                              )),
+                              style: ElevatedButton.styleFrom(
+                              primary: ThemeInfo.bottomTabButtonColor,
+                              shadowColor:Colors.white
+                              ),
+                            ),
+                  
+                            SizedBox(width: 50.0),
+                  
+                            ElevatedButton(
+                              onPressed: () async{
+                                //Navigator.push(context, MaterialPageRoute(builder : (context) => SignUp()));
+                              },
+                              child: Text('  Sign Up  ',style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black,
+                              )),
+                              style: ElevatedButton.styleFrom(
+                              primary: ThemeInfo.bottomTabButtonColor,
+                              shadowColor:Colors.white
+                              ),
+                            )
+                          ],
+                        ),
+                      
+                  
+                                  
+                  
+                        SizedBox(height: 10,),
+                                  Positioned(
+                    bottom: -15,
+                    child: Container(
+                      width: MediaQuery.of(context).size.width,
+                      alignment: Alignment.center,
+                      child: ElevatedButton(
+                        onPressed: () async {
+                          await FirebaseServices().signInWithGoogle();
+                          print("Hello");
+                          Navigator.of(context).push(CustomPageRoute(
+                              //child: DashboardLayout(),
+                              //child: AdditionalSettingsScreen(),
+                              child: RegisterPage(),
+                              transition: "slide right"));
+                        },
+                        child: const Padding(
+                          padding: EdgeInsets.symmetric(
+                              vertical: 15.0, horizontal: 10),
+                          child: Text(
+                            "Sign in with Google",
+                            style: TextStyle(fontSize: 20),
+                          ),
+                        ),
+                        
                       ),
+                    ),
+                                  ),
+                                  ],
                     ),
                   ),
                 ),
               ],
             ),
-            flex: 10,
+            flex: 40,
           ),
+         
+        
+        
+            
+         
+
           Expanded(
             child: Container(
               padding: EdgeInsets.symmetric(horizontal: 20),
               child: Row(
                 children: const [
+                  
                   Text(
                     "Terms & Conditions",
                     style: TextStyle(
@@ -107,5 +160,6 @@ class LoginScreen extends StatelessWidget {
         ],
       ),
     );
+    
   }
 }
