8e34edca4b29c45e752866bb75451d8fd17d0f9c
Thanoraj Muthulingam <90021137+Thanoraj@users.noreply.github.com>
2022-07-15 14:53:05 +0530

    Code refactoring

---
+++ lib/Models/added_food_model.dart
@@ -0,0 +1,10 @@
+class MealModel {
+  final String food;
+  final int amount;
+  final String mealType;
+  MealModel({
+    required this.food,
+    required this.amount,
+    required this.mealType,
+  });
+}
--- lib/Models/food_model.dart
+++
@@ -1,5 +0,0 @@
-class MealModel {
-  late final String food;
-  late final int amount;
-  late final String mealType;
-}
