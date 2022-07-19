5b779c1a54c991505bcd8a8612ee52f1507ffba7
Thanoraj Muthulingam <90021137+Thanoraj@users.noreply.github.com>
2022-07-19 11:45:46 +0530

    Dependencies upgradation

--- lib/Services/IamageStoreService.dart
+++ lib/Services/IamageStoreService.dart
@@ -4,7 +4,6 @@ import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart' as firebase_core;
-import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 import 'package:firebase_storage/firebase_storage.dart';
 
@@ -12,8 +11,6 @@ class imageStorage {
   User? user = FirebaseAuth.instance.currentUser;
   var email = "";
   var id;
-
-  DatabaseReference userImage = FirebaseDatabase.instance.ref('users_Images');
 
   final firebase_storage.FirebaseStorage storage =
       firebase_storage.FirebaseStorage.instance;
