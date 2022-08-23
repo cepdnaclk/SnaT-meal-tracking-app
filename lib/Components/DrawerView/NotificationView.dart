f92290c0fdcea3213d9237c2e69ea1701af16b8e
Hari25483 <haricodmw@gmail.com>
2022-08-23 22:15:34 +0530

    Get missed meal times of yesterday for notifications

--- lib/Components/DrawerView/NotificationView.dart
+++ lib/Components/DrawerView/NotificationView.dart
@@ -2,6 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:mobile_app/Pages/dashboard_layout.dart';
 import 'package:mobile_app/Settings/notification_service.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:mobile_app/constants.dart';
+
+User? user = FirebaseAuth.instance.currentUser;
 
 class NotificationView extends StatefulWidget {
   const NotificationView({Key? key}) : super(key: key);
@@ -13,9 +18,51 @@ class NotificationView extends StatefulWidget {
 class _NotificationViewState extends State<NotificationView> {
   late final NotificationService service;
 
+  void getMissingFoodData() {
+    user = FirebaseAuth.instance.currentUser;
+    String uid;
+
+    if (user != null) {
+      uid = user!.uid;
+    } else {
+      uid = '';
+    }
+    DateTime now = DateTime.now();
+    final yesterday = DateTime(now.year, now.month, now.day - 1);
+    final yesterday_date = yesterday.toString().substring(0, 10);
+    getYesterdayMissedFoodCategories(yesterday_date);
+  }
+
+  getYesterdayMissedFoodCategories(String yesterday_date) async {
+    await FirebaseFirestore.instance
+        .collection("users")
+        .doc(user!.uid)
+        .collection('foodLog')
+        .doc(yesterday_date)
+        .get()
+        .then((DocumentSnapshot documentSnapshot) {
+      if (documentSnapshot.exists) {
+        Map numMap = (documentSnapshot.data() as Map);
+        var consumedMealTimes = <String>{};
+        for (var v in numMap.keys) {
+          print(v.toString());
+          consumedMealTimes.add(v);
+        }
+        final List<String> consumedFood = consumedMealTimes.toList();
+        List<String> difference =
+            mealTimes.toSet().difference(consumedFood.toSet()).toList();
+        print(difference.toString());
+      } else {
+        print(
+            "You didn't keep track of Mealtimes yesterday. Please keep track of your Mealtimes");
+      }
+    });
+  }
+
   @override
   void initState() {
     service = NotificationService();
+    getMissingFoodData();
     service.initialize();
     listenToNotification();
     super.initState();
@@ -31,7 +78,9 @@ class _NotificationViewState extends State<NotificationView> {
             ElevatedButton(
               onPressed: () async {
                 await service.showNotification(
-                    id: 0, title: 'General Notification', body: 'Just a Notification');
+                    id: 0,
+                    title: 'General Notification',
+                    body: 'Just a Notification');
               },
               child: const Text('show notification'),
             ),
@@ -49,10 +98,10 @@ class _NotificationViewState extends State<NotificationView> {
             ElevatedButton(
               onPressed: () async {
                 await service.showDailyNotification(
-                    id: 1,
-                    title: 'daily notification',
-                    body: 'Time is 9.52 a.m.',
-                    time: const Time(9,52,0),
+                  id: 1,
+                  title: 'daily notification',
+                  body: 'Time is 9.52 a.m.',
+                  time: const Time(9, 52, 0),
                 );
               },
               child: const Text('show daily notification'),
@@ -86,10 +135,8 @@ class _NotificationViewState extends State<NotificationView> {
     if (payload != null && payload.isNotEmpty) {
       print('payload $payload');
 
-      Navigator.push(
-          context,
-          MaterialPageRoute(
-              builder: ((context) => DashboardLayout())));
+      Navigator.push(context,
+          MaterialPageRoute(builder: ((context) => DashboardLayout())));
     }
   }
 }
--- lib/Settings/notification_service.dart
+++ lib/Settings/notification_service.dart
@@ -94,7 +94,7 @@ class NotificationService {
       id,
       title,
       body,
-      _scheduleDaily(Time(15,31,0)), // daily notification time
+      _scheduleDaily(Time(15, 31, 0)), // daily notification time
       details,
       payload: payload,
       androidAllowWhileIdle: true,
@@ -143,5 +143,4 @@ class NotificationService {
   Future<void> cancelNotifications(int id) async {
     await _notificationService.cancel(id);
   }
-
 }
